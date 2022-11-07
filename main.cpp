418ba002fb17dea6220ca4ff2fd47e1524b158d1
Jaipikun <maska989@gmail.com>
2022-11-07 20:44:38 +0100

    Changed some things & generated data

--- main.cpp
+++ main.cpp
@@ -4,8 +4,14 @@
 
 using namespace std;
 
+//////
+
+
+
+//////
+
 double test_function(double x){
-    return pow(x,2);
+    return pow(2,pow(x,2)+1);
 }
 
 double derivative_1(double x,double h,double (*function)(double)){
@@ -13,9 +19,22 @@ double derivative_1(double x,double h,double (*function)(double)){
     return deriv;
 }
 
-
+double derivative_2(double x,double h,double (*function)(double)){
+    double deriv = (function(x+h)-function(x-h)-(2*function(x)))/pow(h,2);
+    return deriv;
+}
 
 int main(){
-    cout<<derivative_1(3,1,test_function);
+    ofstream data;
+    ofstream data2;
+    data.open("data.txt");
+    data2.open("data2.txt");
+    for(double h = 0.1;h>pow(10,-17);h*=0.1){
+        //cout<<h<<endl;
+        data<<derivative_1(3,h,test_function)<<"\t"<<h<<endl;
+        data2<<derivative_2(3,h,test_function)<<"\t"<<h<<endl;
+    }
+    data.close();
+    data2.close();
     return 0;
 }
