7245c6b7cad613aa03d8e1ea907c57d0e90ca86e
Jaipikun <maska989@gmail.com>
2022-11-08 12:36:54 +0100

    Scaled h with log10 to simplify ploting

--- main.cpp
+++ main.cpp
@@ -4,36 +4,36 @@
 
 using namespace std;
 
-//////
-
-
-
-//////
-
-double test_function(double x){
-    return pow(2,pow(x,2)+1);
+double test_function(double x)
+{
+    return pow(5,x);
 }
 
-double derivative_1(double x,double h,double (*function)(double)){
+double derivative_1(double x,double h,double (*function)(double))
+{
     double deriv = (function(x+h)-function(x-h))/(2*h);
     return deriv;
 }
 
-double derivative_2(double x,double h,double (*function)(double)){
+double derivative_2(double x,double h,double (*function)(double))
+{
     double deriv = (function(x+h)-function(x-h)-(2*function(x)))/pow(h,2);
     return deriv;
 }
 
-int main(){
+int main()
+{
     ofstream data;
     ofstream data2;
     data.open("data.txt");
     data2.open("data2.txt");
-    for(double h = 0.1;h>pow(10,-17);h*=0.1){
-        //cout<<h<<endl;
-        data<<derivative_1(3,h,test_function)<<"\t"<<h<<endl;
-        data2<<derivative_2(3,h,test_function)<<"\t"<<h<<endl;
+    
+    for(double h = 0.1;h>pow(10,-17);h*=0.1)
+    {
+        data<<derivative_1(3,h,test_function)<<"\t"<<-log10(h)<<endl;
+        data2<<derivative_2(3,h,test_function)<<"\t"<<-log10(h)<<endl;
     }
+
     data.close();
     data2.close();
     return 0;
